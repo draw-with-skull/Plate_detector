3a74ea4fafbe856f7590c85f3c5639c16a241248
Razvi <drawwithskull@gmail.com>
2022-01-10 22:57:16 +0200

    Save Funtion

--- Plate_detector/Main.cpp
+++ Plate_detector/Main.cpp
@@ -2,11 +2,12 @@
 
 int main(int argc, char** argv) {
 	std::string xml_path = "haarcascade_frontalface_default.xml";
-	std::string data_path = "img1";
+	std::string data_path = "img1.png";
 
 	Detect detect(xml_path,data_path);
-	detect.run(WEBCAM);
-	//detect.display();
+	detect.run(PHOTO);
+	detect.set_save_settings(true, true);
+	detect.save();
 	//printf("************************************************************************************************");
 	cv::waitKey(0);
 
